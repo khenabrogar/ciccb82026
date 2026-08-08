c43ac8505b086599923b8ca1b6163b0dc9bc5317
khenabrogar <khenabrogar@gmail.com>
2026-08-08 02:04:29 +0000

    Done

---
+++ Task2.java
@@ -0,0 +1,12 @@
+public class Task2 {
+    public static void main(String[] args) {
+        byte bVal = 31;
+        short sVal = 1;
+        int iVal = 10;
+        float fVal = 2.0f;
+        boolean isActive = true;
+        char cVal = 'H';
+
+        System.out.println(cVal + "" + bVal + sVal + iVal + " w0r1d " + fVal + " " + isActive);
+    }
+}
